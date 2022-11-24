f298360b9f2fd2840a42d696f4ff8484e7c5aa5b
LouiseVaes <118883304+LouiseVaes@users.noreply.github.com>
2022-11-24 11:22:18 +0100

    Create Main_test.R

---
+++ Main_test.R
@@ -0,0 +1,9 @@
+
+library(tidyverse)
+
+Data <- readRDS("C:/Users/LoVa3397/Downloads/test_df (3).Rdata")
+
+Startperiod <- "2022-01-01"
+Endperiod   <- "2022-07-31"
+
+Data_long <- gather(Data, vaccin, day,date.vacc1:date.vacc2, factor_key=TRUE)
