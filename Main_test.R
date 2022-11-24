73f33bb2d20a7911fd950cf2712522aee8d2420b
LouiseVaes <118883304+LouiseVaes@users.noreply.github.com>
2022-11-24 11:39:06 +0100

    Update Main_test.R
    Main script

--- Main_test.R
+++ Main_test.R
@@ -7,3 +7,4 @@ Startperiod <- "2022-01-01"
 Endperiod   <- "2022-07-31"
 
 Data_long <- gather(Data, vaccin, day,date.vacc1:date.vacc2, factor_key=TRUE)
+Data_long <- Data_long[complete.cases(Data_long),]
